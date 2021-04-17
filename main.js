7fe09117559724508f0387db5d8045941007b308
murakami08 <72305301+murakami08@users.noreply.github.com>
2021-04-17 23:45:50 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,47 @@
+$(function () {
+  $('.toggle-btn').on('click', function () {
+    if (!$('.header').hasClass('open')) {
+      $('.header').addClass('open');
+    } else {
+      $('.header').removeClass('open');
+    }
+  });
+  $('.mask').on('click', function () {
+    $('.header').removeClass('open');
+  });
+
+  // ===================================================
+  // ページ内リンクのイベント
+  $('a[href^="."]').click(function () {
+    // リンクを取得
+    let href = $(this).attr("href");
+    // ジャンプ先のid名をセット
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    // トップからジャンプ先の要素までの距離を取得
+    let position = target.offset().top;
+    // animateでスムーススクロールを行う
+    // 600はスクロール速度で単位はミリ秒
+    $("html, body").animate({ scrollTop: position }, 600, "swing");
+    return false;
+  });
+
+  // スクロール時のイベント
+  $(window).scroll(function () {
+    // fadeinクラスに対して順に処理を行う
+    $('.fadein').each(function () {
+      // スクロールした距離
+      let scroll = $(window).scrollTop();
+      // fadeinクラスの要素までの距離
+      let target = $(this).offset().top;
+      // 画面の高さ
+      let windowHeight = $(window).height();
+      // fadeinクラスの要素が画面下にきてから200px通過
+      // 通過したタイミングで要素を表示
+      if (scroll > target - windowHeight + 200) {
+        $(this).css('opacity', '1');
+        $(this).css('transform','translateY(0)');
+      }
+    });
+  });
+
+});
